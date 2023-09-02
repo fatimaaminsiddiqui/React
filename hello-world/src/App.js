36260ad26f867da76f6ed602247e8014f335cfc0
fatimaaminsiddiqui <siddiquifatima04@gmail.com>
2023-09-01 23:30:55 -0700

    new commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,13 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      <Counter></Counter>
+      {/* <Greet name = "Bruce" heroName = "Batman">
+      <p>This is children props</p>
+      </Greet>
+      <Greet name = "Clark" heroName = "Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name = "Diana" heroName = "Wonder Woman"></Greet> */}
+      {/* <Welcome /> */}
     </div>
   );
 }
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -4,6 +4,13 @@ import React from 'react'
 // return <h1>Hello Fatima</h1>
 //}
 
- const Greet = () => <h1>Hello Fatima</h1>
+ const Greet = props => {
+  const {name, heroName} = props
+  return (
+    <div>
+      <h1>Hello {name} the {heroName}</h1>
+    </div>
+  )
+ }
 
 export default Greet 
